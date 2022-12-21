d320e4ac56881c383bc712be36be4327d3430e38
Pavitar Kumar <pavitrsheoran003@gmail.com>
2022-12-21 20:16:47 +0530

    Created FullScreen Component

---
+++ components/FullScreen.js
@@ -0,0 +1,10 @@
+
+function FullScreen({children}) {
+  return (
+    <div className="min-h-screen min-w-screen flex justify-center items-center gap-4">
+        {children}
+    </div>
+  )
+}
+
+export default FullScreen
